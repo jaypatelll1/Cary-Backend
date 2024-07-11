ce9a9a0bacbf4cbdac29ca99726cbb92a346f2c6
jay <pateljay8886@gmail.com>
2024-07-11 20:44:57 +0530

    Update MongoDB connection configuration

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -12,8 +11,7 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
